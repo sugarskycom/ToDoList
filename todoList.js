c0f1d45c28b89506e147cc967d963c8b6930c14c
sugarskycom <779642441@qq.com>
2017-05-01 18:15:37 +0800

    自己写的toDoList

---
+++ todoList.js
@@ -0,0 +1,69 @@
+/**
+ * Created by Administrator on 2017/4/28.
+ */
+var vm = new Vue({
+    el : '#app',
+    data : {
+        todos : [],
+        editTodo : '',
+        state : '',
+        currentTodo : ''
+    },
+    methods:{
+        addTodo(){
+       this.todos.push({titles:this.editTodo,isChecked:false});
+       this.editTodo = '';
+      },
+        changeTitle(todo){
+          return this.currentTodo = todo;
+        },
+        remove(todo){
+          this.todos = this.todos.filter(function (item) {
+              return todo != item;
+          });
+        },
+        reset(){
+            this.currentTodo = '';
+        }
+    },
+    directives:{
+        autoFocus(ele,bindings){
+            if(bindings.value){
+                ele.focus();
+            }
+        }
+    },
+    watch:{
+      todos:{
+          handler(){
+            localStorage.setItem('todos',JSON.stringify(this.todos));
+          },
+          deep:true
+      }
+    },
+    mounted(){
+        this.todos = JSON.parse(localStorage.getItem('todos')) || [];
+    },
+    computed:{
+        cloneTodo(){
+            if(this.state=='complete'){
+                return this.todos;
+            }
+            if(this.state=='finish'){
+                return this.todos.filter(function (item) {
+                   return item.isChecked;
+                });
+            }
+            if(this.state=='unfinish'){
+                return this.todos.filter(function (item) {
+                    return !item.isChecked;
+                });
+            }
+        }
+    }
+});
+var listener = function () {
+    vm.state = window.location.hash.slice(1) || 'complete';
+};
+listener();
+window.addEventListener('hashchange',listener,false);
